33c07404d5333a99628c8ec0d0dbe9847d6c80dc
Lisa Marie Moreno <108356315+lisamarie245@users.noreply.github.com>
2024-11-14 23:17:55 +0000

    Domain-Names-Generator-lisamarie245-FP91S

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let com = [".com", ".net", ".io", ".us"];
+
+  let indicepronoun = Math.floor(Math.random() * pronoun.length);
+  let indiceadj = Math.floor(Math.random() * adj.length);
+  let indicenoun = Math.floor(Math.random() * noun.length);
+  let indicecom = Math.floor(Math.random() * com.length);
+
+  document.querySelector("#domain-name").innerHTML =
+    pronoun[indicepronoun] + adj[indiceadj] + noun[indicenoun] + com[indicecom];
 };
